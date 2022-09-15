618c186b47539d902f06efbd6f96345909d5d19a
ForexPro <roma.minihanowa@mail.ru>
2022-09-15 18:24:21 +0400

    find on id

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -1,10 +1,12 @@
 package com.example.restservice;
 
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static ArrayList<Worker> map = new ArrayList<Worker>();
+	private static ArrayList<Worker> list = new ArrayList<Worker>();
 
 	@PostMapping("/add")
 	public void addWorker(@RequestBody Worker worker) {
 		worker.setUuid(UUID.randomUUID());
-		map.add(worker);
-		System.err.println(worker);
+		list.add(worker);
+//		System.err.println(worker);
 	}
 
 	@GetMapping("/get")
 	public ArrayList<Worker> getWorkers() {
-		return map;
+		return list;
+	}
+
+	@GetMapping("/get/{uuid}")
+	public Worker getWorker(@PathVariable UUID uuid) {
+		Worker worker = list.stream().filter(t -> t.getUuid().equals(uuid)).findFirst()
+				.orElseThrow(() -> new RuntimeException("not found"));
+		System.err.println(worker);
+		return worker;
 	}
 }
